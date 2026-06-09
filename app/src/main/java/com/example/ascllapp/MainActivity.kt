3a99e6e2c0f49deea0b09f80a98a0cb68c7bcfb1
lydia205 <lydiaomombo35@gmail.com>
2026-06-09 03:46:32 -0700

    added folder and create a new project

---
+++ app/src/main/java/com/example/ascllapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.ascllapp
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        //declared my variable
+        var sart = ""
+
+        // get my textview come from activity_main
+        val txtDisplay = findViewById<TextView>(R.id.txtDisplay)
+
+        // dec
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
